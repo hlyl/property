a9565bcd981f99046aaf5a6c6a658a98ba47f80e
hlyl <hlyl@novonordisk.com>
2023-05-02 05:30:42 +0000

    Color-category of icon

--- flask_map.py
+++ flask_map.py
@@ -3,6 +3,8 @@ import folium
 import sqlite3
 import dao
 from dao import Property
+from sqlalchemy import case
+
 from sqlmodel import Field, create_engine, Session, select, update
 import sys
 
@@ -19,8 +21,31 @@ def index():
     #44.71528006921618, 10.74521045661985
 
     with Session(db_engine) as session:
-        #statement = select(Property.id,Property.price, Property.latitude, Property.longitude)
-        statement = select(Property.id, Property.price, Property.latitude, Property.longitude).where(Property.reviewed == 0)
+        price_category = case(
+                [
+                    (Property.price.between(20000, 35000), 'Low'),
+                    (Property.price.between(35001, 50000), 'Mid'),
+                    (Property.price.between(50001, 75000), 'High')
+                ],
+                else_='Unknown'
+            )
+        def get_icon_color(price_category):
+            if price_category == 'Low':
+                return 'green'
+            elif price_category == 'Mid':
+                return 'orange'
+            elif price_category == 'High':
+                return 'red'
+            else:
+                return 'blue'
+
+        statement = select(
+            Property.id,
+            Property.price,
+            Property.latitude,
+            Property.longitude,
+            price_category.label('price_category')
+        ).where((Property.reviewed == 0) & (Property.sold == 0))
         data = session.execute(statement)
 
         for item in data:
@@ -32,9 +57,10 @@ def index():
             <a href="{mapsgoogle}" target="_blank">GoogleMaps<br>\
             {btn}</a>'
             popup = folium.Popup(popup_text, max_width=250)
+            icon_color = get_icon_color(item.price_category)
             marker = folium.Marker(location=[float(item.latitude), float(item.longitude)],
                             popup=popup,
-                            icon = folium.Icon(color='blue', icon_size=(20,20)))
+                            icon = folium.Icon(color=icon_color, icon_size=(20,20)))
             marker.add_to(m)
     print('This is error output', file=sys.stderr)
     print('This is standard output', file=sys.stdout)
